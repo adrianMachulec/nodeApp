561c11277d37935e469fefcd356d7a87b538219a
Adrian Machulec <adriandamianmachulec@gmail.com>
2023-02-22 12:45:41 +0100

    url params

--- index.js
+++ index.js
@@ -7,8 +7,20 @@ app.get('/', (req, res) => {
     res.send('Hello Express!')
 })
 
-app.get('/firmy/tworcastron', (req, res) => {
-    res.send('Hello kontakt')
+app.get('/firmy/:name', (req, res) => {
+    const { name } = req.params
+    const comapnies = [
+        { slug: 'tworcastron', name: 'Tworca Stron.pl' },
+        { slug: 'brukmode', name: 'Bruk Mode' }
+    ]
+
+    const company = comapnies.find(x => x.slug === name)
+    
+    if(company){
+        res.send(`Nazwa firmy: ${company.name}`)
+    } else {
+        res.send(`Nie znaleziono firmy`)
+    }
 })
 
 app.listen(port)
